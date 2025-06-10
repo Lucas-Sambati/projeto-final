c532b69bd13cac517564db26dcb9de9980fecb12
Lucas-Sambati <lucasleonardosambati@hotmail.com>
2025-06-10 11:25:48 -0300

    Bignumbers

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ from db.banco import create_db
 
 def main():
     create_db()
+    st.set_page_config(layout="wide")
+    st.markdown(
+        """
+        <style>
+        .stApp { background-color: #F5F7FA; color: #212121; }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    
     home_page_sleep = st.Page("frontend/sono/home.py", title="Dashboard", icon="🏠", default=True)
-    analise_perfil = st.Page("frontend/sono/analise_perfil.py", title="Ánalise de Perfil", url_path="/sono/analise")
-    correlacoes = st.Page("frontend/sono/correlacoes.py", title="Correlações Entre Sono e Estilo de Vida", url_path="/sono/correlacoes")
-    impactos = st.Page("frontend/sono/impactos.py", title="Impacto da Profissão", url_path="/sono/impactos")
+    analise_perfil = st.Page("frontend/sono/analise_perfil.py", title="Ánalise de Perfil", url_path="sono-analise")
+    correlacoes = st.Page("frontend/sono/correlacoes.py", title="Correlações Entre Sono e Estilo de Vida", url_path="sono-correlacoes")
+    impactos = st.Page("frontend/sono/impactos.py", title="Impacto da Profissão", url_path="sono-impactos")
 
-    home_page_anac = st.Page("frontend/anac/home.py", title="Dashboard", icon="🏠", url_path="/anac/home")
+    home_page_anac = st.Page("frontend/anac/home.py", title="Dashboard", icon="🏠", url_path="anac-home")
     pages = {
-        "Sono": [home_page_sleep, analise_perfil, correlacoes, impactos],
+        "SONO": [home_page_sleep, analise_perfil, correlacoes, impactos],
         "ANAC": [home_page_anac]
     }
     pg = st.navigation(pages)
--- utils/sono/home.py
+++ utils/sono/home.py
@@ -18,4 +18,64 @@ def boxplot_profissao_stress():
     sns.boxplot(data=df, y="nivel_estresse", x="profissao")
     plt.xticks(rotation=45)
     st.pyplot(fig)
+
+def profissao_mais_estressante():
+    return 'Nurse'
+
+def qtd_pessoas_analisadas():
+    df = execute_query("SELECT id_pessoas FROM pessoas;", return_df=True)
+    return df['id_pessoas'].count()
+
+def media_duracao_sono():
+    df = execute_query("SELECT duracao_sono FROM pessoas;", return_df=True)
     
+    return int(df['duracao_sono'].mean())
+
+def prevalencia_disturbios():
+    df = execute_query('SELECT condicao_sono FROM pessoas', return_df=True)
+    
+    total_pessoas = len(df)
+    
+    pessoas_normais = len(df[df['condicao_sono'] == 'Normal'])
+    pessoas_com_disturbios = total_pessoas - pessoas_normais
+    porcentagem_disturbios = (pessoas_com_disturbios / total_pessoas) * 100
+    
+    return round(porcentagem_disturbios, 2)  
+
+def pessoas_com_risco_cardiovascular():
+    df = execute_query('SELECT pressao_sanguinea FROM pessoas', return_df=True)
+    
+    total_pessoas = len(df)
+    
+    pessoas_com_risco = 0
+    
+    for pressao in df['pressao_sanguinea']:
+        try:
+            sistolica, diastolica = map(float, pressao.split('/'))
+            
+            if (sistolica >= 140 or diastolica >= 90):  
+                pessoas_com_risco += 1
+        except:
+            continue
+    
+    porcentagem_risco = (pessoas_com_risco / total_pessoas) * 100
+    
+    return round(porcentagem_risco, 2)
+
+def media_idade():
+    df = execute_query("SELECT idade FROM pessoas", return_df=True)
+    
+    return int(df['idade'].mean())
+
+def colored_metric(label, value, color):
+    st.markdown(
+        f"""
+        <div style="font-weight: bold; font-size: 14px; margin-bottom: 8px;">
+            {label}
+        </div>
+        <div style="color: {color}; font-size: 36px; font-weight: 600;">
+            {value}
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
