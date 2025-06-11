54fcdb42956dff9d54c0e34135fb1d1fbee42928
Guilherme Faraco-laptop <guirfv29@gmail.com>
2025-06-11 15:25:27 -0300

    Feat -> front rotas_aeroportos

--- frontend/anac/rotas_aeroportos.py
+++ frontend/anac/rotas_aeroportos.py
@@ -1,13 +1,27 @@
 from utils.anac.rotas_aeroportos import *
 import streamlit as st
 
-st.title("Top 10 Aeroportos por categoria")
-grafico_aeroportos_maior_carga()
-st.write("""
-Vemos que o aeroporto com mais carga transportada foi o Aeroporto Internacional Murtala Muhammed, da Nigéria
-""")
+st.markdown("""
+<style>
+.big-font {
+    margin: auto;
+    font-size:24px !important;
+    padding:0px 48px 16px 48px;
+    text-align:justify;
+    align-items: center;
+    align-content: center;
+    font-weight: bold;
+}
+</style>
+""", unsafe_allow_html=True)
 
-grafico_aeroportos_mais_passageiros()
-st.write("""
-Vemos que o aeroporto com mais passageiros transportados foi o Aeroporto Internacional de Hamad, do Catar
-""")
+st.title("Top 10 Aeroportos por categoria")
+
+col1, col2 = st.columns(2, gap='medium', border=True)
+with col1:
+    grafico_aeroportos_maior_carga()
+    st.markdown('<p class="big-font">Vemos que o aeroporto com mais carga transportada foi o Aeroporto Internacional Murtala Muhammed, da Nigéria</p>', unsafe_allow_html=True)
+
+with col2:
+    grafico_aeroportos_mais_passageiros()
+    st.markdown('<p class="big-font">Vemos que o aeroporto com mais passageiros transportados foi o Aeroporto Internacional de Hamad, do Catar</p>', unsafe_allow_html=True)
