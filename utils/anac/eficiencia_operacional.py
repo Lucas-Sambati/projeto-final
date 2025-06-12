419cca1040acb3bd0649b5540f663be6d6008574
NetoLanches <ruineto0303@gmail.com>
2025-06-12 12:01:40 -0300

    Eficiencia

--- utils/anac/eficiencia_operacional.py
+++ utils/anac/eficiencia_operacional.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from db.anac.banco import execute_query
 from io import BytesIO
+import numpy as np
 
 @st.cache_data
 def grafico_combustivel_voos():
@@ -44,7 +45,48 @@ def grafico_horas_passageiros():
     st.image(buf, width=1000)  
 
     plt.close(fig)
-    
 
+@st.cache_data
+def atk_rtk():    
+    df = execute_query(
+        "SELECT sigla_empresa, atk, rtk, decolagens FROM viagens "
+        "WHERE atk IS NOT NULL AND rtk IS NOT NULL AND decolagens IS NOT NULL",
+        return_df=True
+    )
 
-    
+    df = df[(df['atk']>5000000) & (df['rtk'] >5000000)]
+    df_sorted = df.sort_values('decolagens', ascending=False)
+    df_top = df_sorted.drop_duplicates(subset='sigla_empresa').head(10)
+
+    labels = df_top['sigla_empresa']
+    atk = df_top['atk']
+    rtk = df_top['rtk']
+
+    x = np.arange(len(labels))  # posições no eixo X
+    width = 0.35  # largura das barras
+
+    fig, ax = plt.subplots(figsize=(12,7))
+
+    # Barras lado a lado
+    bar1 = ax.bar(x - width/2, atk, width, label='ATK', color='red')
+    bar2 = ax.bar(x + width/2, rtk, width, label='RTK', color='blue')
+
+    # Configurações do eixo X
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels, rotation=45, ha='right')
+
+    # Títulos e legendas
+    ax.set_title('Top 10 Empresas com Mais RPK (Revenue Passenger Kilometers)', fontsize=16)
+    ax.set_xlabel('Empresa', fontsize=12)
+    ax.set_ylabel('ATK e RTK', fontsize=12)
+    ax.legend()
+
+    sns.despine()
+    plt.tight_layout()
+
+    # Exibe no Streamlit
+    st.pyplot(fig)
+    st.write('''
+    Barra vermelha (ATK) Capacidade disponível total\n
+    Barra azul (RTK) Demanda efetiva
+    ''')
