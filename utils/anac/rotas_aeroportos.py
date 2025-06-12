0d88d250b4c83cdadbcc21bcf13a0f92d9e3bff4
Giovani Bruno Correa <giovani.correa@edu.unifil.br>
2025-06-12 11:25:46 -0300

    Atualização rotas e aeroportos 	modified:   utils/anac/rotas_aeroportos.py

--- utils/anac/rotas_aeroportos.py
+++ utils/anac/rotas_aeroportos.py
@@ -5,6 +5,21 @@ import matplotlib.pyplot as plt
 from db.anac.banco import execute_query
 
 @st.cache_data
+def grafico_aeroportos_decolagens():
+    df = execute_query("SELECT aeroporto_origem_sigla, natureza, SUM(decolagens) AS decolagens FROM viagens GROUP BY aeroporto_origem_sigla, natureza ORDER BY decolagens DESC", return_df=True)
+
+    total_decolagens = df.groupby('aeroporto_origem_sigla')['decolagens'].sum().reset_index()
+    top_empresas = total_decolagens.nlargest(10, 'decolagens')['aeroporto_origem_sigla']
+    df_top = df[df['aeroporto_origem_sigla'].isin(top_empresas)]
+
+    fig, ax = plt.subplots(figsize=(10,4))
+    sns.barplot( y='aeroporto_origem_sigla', x='decolagens', hue='natureza', data=df_top, ax=ax, order=top_empresas, palette="viridis")
+    ax.set_title('Top 10 Aeroportos com Mais Decolagens', fontsize=16)
+    ax.set_xlabel('Quantidade de Decolagens', fontsize=12)
+    ax.set_ylabel('Aeroportos', fontsize=12)
+    st.pyplot(fig)
+
+@st.cache_data
 def grafico_aeroportos_maior_carga():
     df = execute_query("""
         SELECT aeroporto_origem_sigla, carga_paga AS carga_total
